4fc3642e872aae3d8d3c46bb4e60f2831e041798
Prince Dennis <dennisprince815@gmail.com>
2024-02-14 13:16:04 +0100

    recently

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -36,10 +36,10 @@ const  Benefits = ({setSelectedPage}: Props) =>  {
         initial="hidden" whileInView="visible"
         viewport={{ once: true, amount: 0.5 }}
         transition={{duration: 0.5 }}
-        variants={{
-            hidden: { opacity: 0, x: -50 },
-            visible: { opacity: 1, x: 0 }
-        }}
+        // variants={{
+        //     hidden: { opacity: 0, x: -50 },
+        //     visible: { opacity: 1, x: 0 }
+        // }}
        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}>
         {/* header */}
         <div className="md:my-5 md:w-3/5" >

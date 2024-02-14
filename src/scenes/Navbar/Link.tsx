4e1846dabcb576e9b42d594be8ea0b805c4e8c7d
Prince Dennis <dennisprince815@gmail.com>
2024-02-14 13:04:12 +0100

    recntly

--- src/scenes/Navbar/Link.tsx
+++ src/scenes/Navbar/Link.tsx
@@ -4,7 +4,7 @@ import AnchorLink from "react-anchor-link-smooth-scroll"
 type Props = {
     page: string;
     selectedPage: SelectedPage;
-    setSelectedPage: (value: string) => void;
+    setSelectedPage: (value: SelectedPage) => void;
 }
 
 const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
